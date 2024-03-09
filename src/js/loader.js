6456ea608d3f377eb1325a9918a884c937030d23
TheCreatorrr333 <thecreatorrr333@gmail.com>
2024-03-09 16:00:21 +0100

    tails

--- src/js/loader.js
+++
@@ -1,5 +0,0 @@
-if (typeof jQuery === 'undefined') {
-  var script = document.createElement('script');
-  script.src = 'https://code.jquery.com/jquery-3.6.0.min.js';
-  document.head.appendChild(script);
-}
--- src/js/navbar.js
+++ src/js/navbar.js
@@ -1,29 +1,68 @@
-let navbar_nav = document.createElement("nav");
-let navbar_div1 = document.createElement("div");
-let navbar_div = document.createElement("div");
-navbar_div1.appendChild(navbar_div)
+{
+  let nav = document.createElement("nav");
 
-const navbar_links = [
-  ["Strona Główna", "index.html"],
-  ["Galeria", "src/html/gallery.html"],
-  ["Test", "src/html/pages/test.html"],
-  ["Test", "src/html/pages/test.html"],
-  ["Test", "src/html/pages/test.html"],
-]
+  let outer_div = document.createElement("div");
+  outer_div.appendChild(nav)
 
-navbar_links.forEach(page => {
-  const navbar_a = document.createElement('a');
-  navbar_a.textContent = page[0];
-  navbar_a.className = "grower-link";
-  navbar_a.href = page[1];
-  navbar_div.appendChild(navbar_a);
-})
+  outer_div.style.display = 'flex';
+  outer_div.style.flexDirection = 'row';
+  outer_div.style.alignItems = 'center'
+  outer_div.style.gap = '10px'
 
-navbar_div.className = "navdiv simple-layout-item"; // + "grower-x-small grower-noshrink"
-navbar_div.style.display = "flex";
-navbar_div.style.flexWrap = "wrap";
+  {
+    let div = document.createElement("div");
 
-navbar_nav.appendChild(navbar_div);
+    div.className = "simple-image-item grower";
 
-const navbar_script = document.currentScript;
-navbar_script.parentNode.replaceChild(navbar_nav, navbar_script);
+    let img = document.createElement("img");
+    img.src = 'assets/img/tails.png';
+    img.style.width = '100px';
+    div.appendChild(img)
+
+    outer_div.appendChild(div)
+  }
+
+  {
+    let div = document.createElement("div");
+
+    const navbar_links = [
+      ["Strona Główna", "index.html"],
+      ["Galeria", "src/html/gallery.html"],
+      ["Test", "src/html/pages/test.html"],
+      ["Test", "src/html/pages/test.html"],
+      ["Test", "src/html/pages/test.html"],
+    ]
+
+    navbar_links.forEach(page => {
+      const a = document.createElement('a');
+      a.textContent = page[0];
+      a.className = "grower-link";
+      a.href = page[1];
+      div.appendChild(a);
+    })
+
+    div.className = "navdiv simple-layout-item";
+    div.style.display = "flex";
+    div.style.flexWrap = "wrap";
+    div.style.flexGrow = "1";
+
+    outer_div.appendChild(div);
+  }
+
+  {
+    let div = document.createElement("div");
+
+    div.className = "simple-image-item grower";
+
+    let img = document.createElement("img");
+    img.src = 'assets/img/tails2.png';
+    img.style.width = '100px';
+    img.style.height = '100px';
+    div.appendChild(img)
+
+    outer_div.appendChild(div)
+  }
+
+  const script = document.currentScript;
+  script.parentNode.replaceChild(outer_div, script);
+}
